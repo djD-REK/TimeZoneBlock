c132277d4ae02b4171b31324c00ac7820fb55463
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-06-04 12:25:42 -0400

    Boilerplate

--- src/Block.js
+++ src/Block.js
@@ -4,7 +4,20 @@ import { getStyles } from "./getStyles"
 import { defaultConfig } from "./configs"
 
 const Block = (props) => {
+  // Get the CSS styles from Aphrodite
   const classes = StyleSheet.create(getStyles(props))
+
+  // Destructure the Element props
+  const { dateRange, text } = props
+  // Destructure the dateRange array
+  const [startDateProp, endDateProp] = dateRange
+  // The dates in VOLT's Site Designer are stored as ISO strings
+  // e.g. "2019-10-05T14:58:00.000Z" so make them into Date objects:
+  const startDate = new Date(startDateProp)
+  const endDate = new Date(endDateProp)
+  // Fetch the current date and time
+  const currentDate = new Date()
+
   return <h1 className={css(classes.example)}>{props.text}</h1>
 }
 
--- src/configs.js
+++ src/configs.js
@@ -1,12 +1,64 @@
 import { ElementPropTypes } from "@volusion/element-proptypes"
 
 export const configSchema = {
+  dateRange: {
+    label: "Sale Dates (Start and End Dates)",
+    type: ElementPropTypes.dateRange,
+  },
   text: {
-    label: "Text content",
+    label: "Text Content",
     type: ElementPropTypes.string,
+  },
+  startTime: {
+    label: "Start Time (on Start Date)",
+    type: ElementPropTypes.slider,
+  },
+  endTime: {
+    label: "End Time (on End Date)",
+    type: ElementPropTypes.slider,
+  },
+  timeZone: {
+    label: "Time Zone (for Start and End Times)",
+    type: ElementPropTypes.slider,
   },
 }
 
+// Set up default configuration for the dateRange picker
+const today = new Date() // start date
+const nextWeek = new Date() // end date
+nextWeek.setDate(nextWeek.getDate() + 7)
+
 export const defaultConfig = {
-  text: "Element Starter Block",
+  dateRange: [today, nextWeek],
+  text: "50% Off All Sale Items",
+  startTime: {
+    labelPrefix: "",
+    labelStepSize: 1,
+    labelSuffix: ":00",
+    min: 0,
+    max: 23,
+    stepSize: 8,
+    selectedValue: 12,
+    vertical: false,
+  },
+  endTime: {
+    labelPrefix: "",
+    labelStepSize: 1,
+    labelSuffix: ":00",
+    min: 0,
+    max: 23,
+    stepSize: 8,
+    selectedValue: 12,
+    vertical: false,
+  },
+  timeZone: {
+    labelPrefix: "",
+    labelStepSize: 10,
+    labelSuffix: ":00UTC",
+    min: -12,
+    max: 12,
+    stepSize: 1,
+    selectedValue: -5,
+    vertical: false,
+  },
 }
